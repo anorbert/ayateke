dfff489a50226539a2324cc95824864ff1edf0d8
anorbert <anorbertdezin@gmail.com>
2018-08-24 08:50:06 -0700

    changed app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
        Schema::defaultStringLength(191);
+
+       if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+
     }
 
     /**
@@ -24,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
